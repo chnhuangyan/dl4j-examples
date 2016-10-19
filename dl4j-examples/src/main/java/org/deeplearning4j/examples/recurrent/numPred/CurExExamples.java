2355a9c36e3c12078b64ba4e73dd1f7465af05a1
YoungH <chnhuangyan@gmail.com>
2016-10-19 14:29:26 -0400

    Initial

---
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/numPred/CurExExamples.java
@@ -0,0 +1,10 @@
+package org.deeplearning4j.examples.recurrent.numPred;
+
+/**
+ * Created by YoungH on 10/19/16.
+ * This example trains a RNN. We take the history data of current exchange rate from CNY to One U.S. dollar as input,
+ * , it will predict the rate of next time.
+ */
+public class CurExExamples {
+
+}
